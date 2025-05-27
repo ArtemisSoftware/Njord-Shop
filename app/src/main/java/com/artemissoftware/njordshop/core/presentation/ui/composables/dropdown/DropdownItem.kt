2ed0bd65c2728ee1e6d251a6a8be539036bbe1d1
gustavo <gustavo.isaac@gmail.com>
2025-05-27 10:10:21 +0100

    DropdownItem

---
+++ app/src/main/java/com/artemissoftware/njordshop/core/presentation/ui/composables/dropdown/DropdownItem.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.njordshop.core.presentation.ui.composables.dropdown
+
+import com.artemissoftware.njordshop.core.presentation.ui.composables.text.UiText
+
+data class DropdownItem(
+    val id: Int,
+    val description: UiText
+)
+
