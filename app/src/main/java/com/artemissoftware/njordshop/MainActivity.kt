7a9fdac37388a7f5128b2681619571aef4bcbb4d
gustavo <gustavo.isaac@gmail.com>
2025-05-27 17:56:22 +0100

    Update on main

--- app/src/main/java/com/artemissoftware/njordshop/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/njordshop/MainActivity.kt
@@ -4,44 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.artemissoftware.njordshop.ui.theme.NjordShopTheme
+import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.njordshop.core.presentation.designsystem.theme.NjordShopTheme
+import com.artemissoftware.njordshop.features.form.presentation.navigation.FormRoute
+import com.artemissoftware.njordshop.navigation.RootNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NjordShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+
+                RootNavGraph(
+                    navController = navController,
+                    startDestination = FormRoute.Validation,
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NjordShopTheme {
-        Greeting("Android")
     }
 }
