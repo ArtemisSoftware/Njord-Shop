669676912683aa0cd88137f83503f4d304d82203
gustavo <gustavo.isaac@gmail.com>
2025-05-27 11:01:13 +0100

    DetailTopBar

---
+++ app/src/main/java/com/artemissoftware/njordshop/features/products/presentation/detail/composables/DetailTopBar.kt
@@ -0,0 +1,59 @@
+package com.artemissoftware.njordshop.features.products.presentation.detail.composables
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.artemissoftware.njordshop.R
+import com.artemissoftware.njordshop.core.presentation.designsystem.theme.NjordShopTheme
+import com.artemissoftware.njordshop.core.presentation.designsystem.theme.ThemePreviews
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun DetailTopBar(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.Transparent
+        ),
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = ""
+                )
+            }
+        },
+        title = {
+            Text(
+                text = stringResource(id = R.string.product_detail),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+            )
+        },
+    )
+}
+
+@ThemePreviews
+@Composable
+private fun DetailTopBarPreview() {
+    NjordShopTheme {
+        DetailTopBar(
+            onBackClick = {},
+        )
+    }
+}
