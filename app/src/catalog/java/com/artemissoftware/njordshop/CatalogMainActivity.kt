1a5e05f730738ac9acff22cd3d8389f51b88b054
gustavo <gustavo.isaac@gmail.com>
2025-05-27 11:04:09 +0100

    CatalogMainActivity

---
+++ app/src/catalog/java/com/artemissoftware/njordshop/CatalogMainActivity.kt
@@ -0,0 +1,30 @@
+package com.artemissoftware.njordshop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.njordshop.core.presentation.designsystem.theme.NjordShopTheme
+import com.artemissoftware.njordshop.features.form.presentation.navigation.FormRoute
+import com.artemissoftware.njordshop.features.products.presentation.navigation.ProductsRoute
+import com.artemissoftware.njordshop.navigation.RootNavGraph
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CatalogMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NjordShopTheme {
+                val navController = rememberNavController()
+
+                RootNavGraph(
+                    navController = navController,
+                    startDestination = ProductsRoute.Catalog,
+                )
+            }
+        }
+    }
+}
