d3ac45e408744dccc83646577e31c78a1440cc48
gustavo <gustavo.isaac@gmail.com>
2025-05-27 11:03:54 +0100

    FormMainActivity

---
+++ app/src/form/java/com/artemissoftware/njordshop/FormMainActivity.kt
@@ -0,0 +1,29 @@
+package com.artemissoftware.njordshop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.njordshop.core.presentation.designsystem.theme.NjordShopTheme
+import com.artemissoftware.njordshop.features.form.presentation.navigation.FormRoute
+import com.artemissoftware.njordshop.navigation.RootNavGraph
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FormMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NjordShopTheme {
+                val navController = rememberNavController()
+
+                RootNavGraph(
+                    navController = navController,
+                    startDestination = FormRoute.Validation,
+                )
+            }
+        }
+    }
+}
