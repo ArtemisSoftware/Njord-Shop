0425abaad45806ca1d122f32b18bb6b9f347e8b3
gustavo <gustavo.isaac@gmail.com>
2025-05-27 09:40:35 +0100

    ThemePreviews

---
+++ app/src/main/java/com/artemissoftware/njordshop/core/presentation/designsystem/theme/ThemePreviews.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.njordshop.core.presentation.designsystem.theme
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Phone - Landscape",
+    device = "spec:width= 411dp, height= 891dp, orientation= landscape, dpi= 420")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class ThemePreviews
