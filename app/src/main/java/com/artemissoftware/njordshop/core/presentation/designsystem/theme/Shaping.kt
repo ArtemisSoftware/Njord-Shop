d11da65f43257a9000e105938546787a04043cb8
gustavo <gustavo.isaac@gmail.com>
2025-05-27 09:40:09 +0100

    Shaping

---
+++ app/src/main/java/com/artemissoftware/njordshop/core/presentation/designsystem/theme/Shaping.kt
@@ -0,0 +1,27 @@
+package com.artemissoftware.njordshop.core.presentation.designsystem.theme
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+
+data class Shaping(
+    val circular: Shape,
+    val roundedCorners0_5: Shape,
+    val roundedCorners2: Shape,
+)
+
+internal val shape = Shaping(
+    circular = CircleShape,
+    roundedCorners0_5 = RoundedCornerShape(4.dp),
+    roundedCorners2 = RoundedCornerShape(16.dp),
+)
+
+internal val localShape = staticCompositionLocalOf<Shaping> { throw IllegalStateException("No Shaping installed") }
+
+val MaterialTheme.shape: Shaping
+    @Composable
+    get() = localShape.current
