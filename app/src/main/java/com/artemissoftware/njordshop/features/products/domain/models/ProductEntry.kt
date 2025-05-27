d93371cdd1b9092d59ecbd76433991335f4b8ed0
gustavo <gustavo.isaac@gmail.com>
2025-05-27 10:16:34 +0100

    ProductEntry

---
+++ app/src/main/java/com/artemissoftware/njordshop/features/products/domain/models/ProductEntry.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.njordshop.features.products.domain.models
+
+data class ProductEntry(
+    val id: Int,
+    val title: String,
+    val thumbnail: String,
+    val rating: Double,
+)
